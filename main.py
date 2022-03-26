0e37d7ca3e8c4f4ad28ef56cf6baea192d6e4902
zhuweiji <55840431+zhuweiji@users.noreply.github.com>
2022-03-26 15:59:31 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+SERVER_IP = get_server_ip()
+SERVER_PORT = get_server_port()
+
+if __name__ == "__main__":
+    WELCOME_MESSAGE = "Welcome To SCSE Bank."
+    MENU_MESSAGE = """----------------------------Bank Menu------------------------------------\n 
+        Please choose a service by typing [1-8]:\n
+        1: Open a new bank account\n
+        2: Query information from a bank account\n
+        3: Deposit to a bank account\n
+        4: Withdraw from a bank account\n
+        5: Monitor update from other accounts\n
+        6: Pay monthly maintenance fee from a bank account\n
+        7: Close a bank account\n
+        8: Print the menu\n
+        0: Stop the client\n"""
+    EXIT_MESSAGE = "Thank you for using SCSE Bank."
+    
+    print(WELCOME_MESSAGE)
+
+    authenticate()
+
+    print(MENU_MESSAGE)
+    while user_selection := input('>'):
+        user_selection = user_selection.strip()
+        if user_selection == '1':
+            pass
+        elif user_selection == '2':
+            pass
+        elif user_selection == '3':
+            pass
+        elif user_selection == '4':
+            pass
+        elif user_selection == '5':
+            pass
+        elif user_selection == '6':
+            pass
+        elif user_selection == '7':
+            pass
+        elif user_selection == '8':
+            pass
+        else:
+            print(f'{user_selection} is invalid. Please try again')
+            print(MENU_MESSAGE)
